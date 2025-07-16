656cd28e917bd0b17d82b551a9337623da76a0e7
SyoinGit <sasasyuga@yahooo.co.jp>
2025-07-16 23:11:25 +0900

    サービスクラスのテストを作成 80%

--- src/test/java/portfolio/shiitake/system/service/ShiitakeServiceTest.java
+++ src/test/java/portfolio/shiitake/system/service/ShiitakeServiceTest.java
@@ -2,6 +2,8 @@ package portfolio.shiitake.system.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -16,9 +18,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import portfolio.shiitake.system.data.staff.Staff;
 import portfolio.shiitake.system.data.staff.StaffDetailDto;
 import portfolio.shiitake.system.data.staff.StaffDto;
 import portfolio.shiitake.system.data.task.TaskDto;
+import portfolio.shiitake.system.data.task.TaskForm;
+import portfolio.shiitake.system.data.task.TaskType;
 import portfolio.shiitake.system.data.worklog.WorkLog;
 import portfolio.shiitake.system.data.worklog.WorkLogDto;
 import portfolio.shiitake.system.repository.ShiitakeRepository;
@@ -63,23 +68,76 @@ class ShiitakeServiceTest {
   }
 
   @Test
-  void 従業員の出勤処理_リポジトリが呼び出されているか確認() {
+  void 従業員情報の単一検索_リポジトリとコンバーターが呼び出されていること() {
     ShiitakeService sut = new ShiitakeService(repository, converter);
-    String loginCode = "staff-001";
-    String staffId = "staff-001";
+    String name = "山田太郎";
+    String loginCode = "staff001";
 
-    WorkLog workLog = getWorklog();
-    workLog.setClockIn(LocalDateTime.now());
-    workLog.setDate(LocalDate.now());
+    Staff staff = new Staff();
+    staff.setName(name);
+    staff.setLoginCode(loginCode);
 
-    when(repository.findStaffId(loginCode)).thenReturn(staffId);
-    sut.clockInStaff(loginCode);
+    List<StaffDto> staffDto = new ArrayList<>();
+    List<WorkLogDto> workLogDto = new ArrayList<>();
+    List<TaskDto> taskDto = new ArrayList<>();
+    List<StaffDetailDto> convert = new ArrayList<>();
 
-    verify(repository, Mockito.times(1)).clockIn(workLog);
+    when(repository.searchStaff(staff)).thenReturn(staffDto);
+    when(repository.findAllWorklog()).thenReturn(workLogDto);
+    when(repository.findAllTask()).thenReturn(taskDto);
+    when(converter.convertStaff(staffDto, workLogDto, taskDto)).thenReturn(convert);
+
+    List<StaffDetailDto> actual = sut.searchStaff(staff);
+
+    verify(repository, Mockito.times(1)).searchStaff(staff);
+    verify(repository, Mockito.times(1)).findAllWorklog();
+    verify(repository, Mockito.times(1)).findAllTask();
+
+    verify(converter, Mockito.times(1)).convertStaff(staffDto, workLogDto, taskDto);
+
+    assertNotNull(actual);
+    assertEquals(actual, convert);
 
   }
 
+  @Test
+  void 従業員の出勤処理_リポジトリが呼び出されているか確認() {
+    ShiitakeService sut = new ShiitakeService(repository, converter);
 
+    String loginCode = "login001";
+    String staffId = "staff-001";
+
+    when(repository.findStaffId(loginCode)).thenReturn(staffId);
+    when(repository.existsWorkLog(anyString())).thenReturn(0);
+    sut.clockInStaff(loginCode);
+    verify(repository).clockIn(any(WorkLog.class)); // 呼ばれたことの確認
+  }
+
+  @Test
+  void 従業員の退勤処理_リポジトリが呼び出されているか確認() {
+    ShiitakeService sut = new ShiitakeService(repository, converter);
+    String loginCode = "login001";
+    String staffId = "staff001";
+
+    when(repository.findStaffId(loginCode)).thenReturn(staffId);
+    when(repository.existsWorkLog(anyString())).thenReturn(1);
+    sut.clockOutStaff(loginCode);
+
+    verify(repository).clockOut(any(WorkLog.class));
+  }
+
+  @Test
+  void 作業の登録_リポジトリが登録されているか確認() {
+    ShiitakeService sut = new ShiitakeService(repository, converter);
+    TaskForm taskForm = new TaskForm();
+    taskForm.setLoginCode("login001");
+    taskForm.setHouseId("staff001");
+    taskForm.setTaskType(String.valueOf(TaskType.CARRY_BLOCKS));
+
+    sut.registerTask(taskForm);
+
+
+  }
 
 
   private WorkLog getWorklog() {
